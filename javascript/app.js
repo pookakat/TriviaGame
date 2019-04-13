6a44fc2117a23681af2b21f30d1e70e0fc9b41aa
Katie Humburg <pookakat@gmail.com>
2019-04-12 23:49:47 -0400

    finish bulk of the remaing style and function tweeks

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,20 @@
-let timeLeft=30;
+let timeLeft = 30;
 let timer;
-let i=1;
+let timeForBonus = 0;
+let i = 1;
 let score = 0;
 let askIt;
 let currentQuestions;
+let correctAnswers = 0;
+let incorrectAnswers = 0;
 
 $( document ).ready(function() {
     $('button').click(function(){
         currentQuestions = questions.slice(0);
-        $('#triviaCard').show();
-        timeLeft=30;
         $('#timer').toggle();
         if ($('button').hasClass("started")){
             clearTimeout(timer);
+            timeLeft = 0;
             $('#triviaCard').hide();
             $('#triviaQuestion').text('');
             i = 1;
@@ -24,28 +26,26 @@ $( document ).ready(function() {
         else{
             $('button').text("Counting Down");
             $('button').addClass("started");
+            $('#otherStuff').removeClass('endGame');
+            $('#otherStuff').hide();
             triviaQuestion();
+            clearTimeout(timer);
+            timeLeft = 30;
+            correctAnswers = 0;
+            incorrectAnswers = 0;
             timer = setInterval(countDown, 1000);
         }
     });
     function countDown(){
         $('#timer').text( timeLeft + ' seconds!');
         timeLeft--;
-        if ( timeLeft <= 0 ){
+        if ( timeLeft <= -1 ){
             $('#timer').html("<p>Time's Up!");
-            clearTimeout(timer);
-            $('button').removeClass( "started" );
-            $('button').text("Try Again?");
-            $('#triviaCard').hide();
-            $('#triviaQuestion').text('');
-            i = 1;
-            $('#score').text('Your total score: ' + score);
-            score = 0;
-        }
+            showTimeOut ();
+            }
     }
     
     function questionMaker(askIt){
-        console.log(askIt);
         const options = askIt.answer.concat(askIt.notOne);
         const $options = $('<ul>');
         while( options.length ) {
@@ -57,8 +57,12 @@ $( document ).ready(function() {
      
 
     function triviaQuestion(){
-        console.log(i);
-        if (i<=20){
+        $('#triviaCard').show();
+        $('#otherStuff').removeClass('win');
+        $('#otherStuff').removeClass('defeat');
+        $('#score').text('Your score is: ' + score);
+        timeLeft=30;
+        if (i<21){
             askIt=getAQuestion(currentQuestions);
             $('#triviaQuestion input:radio').checked = false;
             $('input[type = radio]').attr('disabled', false);
@@ -75,34 +79,78 @@ $( document ).ready(function() {
             });
         }
         else{
-            $('#score').html('<h1>Game Over!</h1>');
-            $('#score').append('Your total score: ' + score);
-            pauseTimer();
+            endGame();
         }
     }
     function checkAnswer(chosen){
         if (chosen === askIt.answer[0]){
-            score += 100;
-            $('#score').text('Right! Your score is now ' + score + '!');
-            timeLeft += 30;
-            $('#triviaQuestion').html('<h2>Right!</h2>');
-            setTimeout(triviaQuestion, 1000);
+            score += 10;
+            showCelebration();
         }
         else{
-            $('#triviaQuestion').html('<h2> Wrong! The answer was: '+ askIt.answer);
-            pauseTimer();
+            showDefeat();
         }
+        pauseTimer();
     }
+    function showCelebration(){
+        $('#triviaCard').hide();
+        $('#otherStuff').show();
+        $('#otherStuff').addClass('win');
+        $('#otherStuff').html('<h1>Correct! HUZZAH!!!</h1>');
+        $('#otherStuff').append('<h2>' + askIt.answer + '</h2>');
+        $('#otherStuff').append('<h2> Your score is now '+ score + '! </h2>');
+        timeForBonus += timeLeft;
+        correctAnswers++;
+    }
+    function showDefeat(){
+        $('#triviaCard').hide();
+        $('#otherStuff').show();
+        $('#otherStuff').addClass('defeat');
+        $('#otherStuff').html('<h1>WRONG!</h1>');
+        $('#otherStuff').append('<h2> The answer is: ' + askIt.answer + '</h2>');
+        $('#otherStuff').append('<h2> Shake it off! Here comes another! </h2>');
+        incorrectAnswers++;
+    }
+
+    function showTimeOut(){
+        $('#triviaCard').hide();
+        $('#otherStuff').show();
+        $('#otherStuff').addClass('defeat');
+        $('#otherStuff').html('<h1>Time OUT!!</h1>');
+        $('#otherStuff').append('<h2> The answer is: ' + askIt.answer + '</h2>');
+        $('#otherStuff').append('<h2> Shake it off! Here comes another! </h2>');
+        pauseTimer();
+    }
+
+    function endGame(){
+        clearTimeout(timer);
+        timer = null;
+        let bonusScore=timeForBonus*3;
+        let totalScore=score+bonusScore;
+        $('button').text("Try Again?");
+        $('button').removeClass("started"); 
+        $('#timer').toggle();
+        $('#triviaCard').hide();
+        $('#otherStuff').show();
+        $('#otherStuff').addClass('endGame');
+        $('#otherStuff').html('<h1>Game Over!</h1>');
+        $('#otherStuff').append('<h2> Good job! You answered <span class="green">'+ correctAnswers +'</span> correcty and <span class="red">'+ incorrectAnswers + '</span> incorrectly out of 20.');
+        $('#otherStuff').append('<h2> Your time left on correct questions was ' + timeForBonus +'seconds! </h2>');
+        $('#otherStuff').append('<h2> Your total score is: '+ totalScore + 'points!</h2>');
+    }
+
     function unclick(clicked){
         clicked.checked = false;
     }
 
     function pauseTimer(){
         clearTimeout(timer);
-        setTimeout(restart, 5000);
+        setTimeout(restart, 3000);
     }
     
     function restart(){
+        $('#otherStuff').hide();
+        $('#otherStuff').empty();
         triviaQuestion();
         timer = setInterval(countDown, 1000);
     }
